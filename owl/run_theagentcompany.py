e9a15d33135dc3c12548a94278358c028d62e12d
Boxuan Li <liboxuan@connect.hku.hk>
2025-05-15 00:33:00 -0700

    Make plane login step more robust

--- owl/run_theagentcompany.py
+++ owl/run_theagentcompany.py
@@ -132,8 +132,13 @@ def pre_login(web_toolkit: WebToolkit, dependencies: List[str]):
             web_toolkit.browser.fill_input_id(find_key_by_tag_name(elements, "input, type=email"), "agent@company.com")
             time.sleep(5)
             elements = web_toolkit.browser.get_interactive_elements()
-            # the next step often fails, let's print out the elements for debugging purposes
-            print(f'elements: {elements}')
+            try:
+                find_key_by_tag_name(elements, "input, type=password")
+            except Exception as e:
+                print(f'Password input not found, try clicking on continue button')
+                web_toolkit.browser.click_id(find_key_by_tag_name(elements, "button"))
+                time.sleep(5)
+                elements = web_toolkit.browser.get_interactive_elements()
             web_toolkit.browser.fill_input_id(find_key_by_tag_name(elements, "input, type=password"), "theagentcompany")
             time.sleep(5)
             assert web_toolkit.browser.get_url() == "http://the-agent-company.com:8091/tac/"
